de3e96399f043eb40d2527480ff35512af1456bd
JMoloughney <github email address>
2018-09-05 19:28:02 -0400

    styling theme of homepage to blue rather blue/green mix

--- personal-website/src/Info.js
+++ personal-website/src/Info.js
@@ -5,8 +5,8 @@ class Info extends React.Component{
 
     return (
     <div className="about">
-      <p id="education"><h1 id="statement">Education</h1></p>
       <p id="interests"><h1 id="statement">Hobbies & Interests</h1></p>
+      <p id="education"><h1 id="statement">Education</h1></p>
       <p id="experiences"><h1 id="statement">Experiences & Achievements</h1></p>
     </div>
     )
