48f569f015b461da1ceff9e5b2345c15d0e575cb
JMoloughney <github email address>
2018-09-05 15:08:15 -0400

    really really basic layout

--- personal-website/src/App.js
+++ personal-website/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Navbar from './Navbar.js'
 
 class App extends Component {
   render() {
@@ -8,11 +9,16 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Jay Zonday</h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          Welcome to Joe Moloughney's corner of the Web
         </p>
+        <p id="mission">An eager and highly motivated full-stack web developer looking to
+        apply my wide range of skills to create and build beautiful
+        and intelligently designed programs that can help change
+        and shape the way we think about tomorrow.</p>
+        <Navbar/>
       </div>
     );
   }
---
+++ personal-website/src/Navbar.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class Navbar extends React.Component{
+  render(){
+
+    return (
+    <div className="navBar">
+      <button className= "navButton" id='projects'>Finished Projects</button>
+      <button className= "navButton" id='ffAdvice' >FF2018 Advice</button>
+      <button className= "navButton" id='resume' >Resume(PDF)</button>
+      <button className= "navButton" id='login' >Contact</button>
+    </div>
+    )
+  }
+}
+
+export default Navbar
