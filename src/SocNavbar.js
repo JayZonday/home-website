46c2dd2a454ea53e5b830494307570fdce9d8fa8
JMoloughney <github email address>
2018-09-13 15:41:27 -0400

    social media linked

--- src/SocNavbar.js
+++ src/SocNavbar.js
@@ -5,10 +5,10 @@ class SocNavbar extends React.Component{
 
     return (
     <div className="navBar">
-      <button className= "navButton" id='facebook'>Facebook</button>
-      <button className= "navButton" id='twitter'>Twitter</button>
-      <button className= "navButton" id='linkedin' >Instagram</button>
-      <button className= "navButton" id='linkedin' >LinkedIn</button>
+      <button className= "navButton" id='facebook'><a href='https://www.facebook.com/JM.KID?ref=br_rs'>Facebook</a></button>
+      <button className= "navButton" id='twitter'><a href='https://twitter.com/jayzonday'>Twitter</a></button>
+      <button className= "navButton" id='linkedin' ><a href='https://www.instagram.com/jayz0nday/'>Instagram</a></button>
+      <button className= "navButton" id='linkedin' ><a href='https://www.linkedin.com/in/josephmoloughney/'>LinkedIn</a></button>
     </div>
     )
   }
