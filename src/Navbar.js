8dee54915a5e438ff716357baae0917f449fc801
JMoloughney <github email address>
2018-09-13 20:33:20 -0400

    more styling and positioning of certain elements on the landing page

--- src/Navbar.js
+++ src/Navbar.js
@@ -5,7 +5,9 @@ class Navbar extends React.Component{
 
     return (
     <div className="navBar">
+      <h2 className="nav">Information Navigation</h2>
       <button className= "navButton" id='projects'><a href='https://github.com/JayZonday'>GitHub Portfolio</a></button>
+      <button className= "navButton" id='youtubes'><a href='https://www.youtube.com/channel/UCPQZKbLIPNYr36qVVAwkI6Q?view_as=subscriber'>YouTube Portfolio</a></button>
       <button className= "navButton" id='ffAdvice'>FF2018 Advice</button>
       <button className= "navButton" id='contact' >Contact</button>
     </div>
--- src/SocNavbar.js
+++ src/SocNavbar.js
@@ -4,11 +4,12 @@ class SocNavbar extends React.Component{
   render(){
 
     return (
-    <div className="navBar">
-      <button className= "navButton" id='facebook'><a href='https://www.facebook.com/JM.KID?ref=br_rs'>Facebook</a></button>
-      <button className= "navButton" id='twitter'><a href='https://twitter.com/jayzonday'>Twitter</a></button>
-      <button className= "navButton" id='linkedin' ><a href='https://www.instagram.com/jayz0nday/'>Instagram</a></button>
-      <button className= "navButton" id='linkedin' ><a href='https://www.linkedin.com/in/josephmoloughney/'>LinkedIn</a></button>
+    <div className="SocNavBar">
+      <h2 className="nav">Social Media</h2>
+      <button className= "SocNavButton" id='facebook'><a href='https://www.facebook.com/JM.KID?ref=br_rs'>Facebook</a></button>
+      <button className= "SocNavButton" id='twitter'><a href='https://twitter.com/jayzonday'>Twitter</a></button>
+      <button className= "SocNavButton" id='instagram' ><a href='https://www.instagram.com/jayz0nday/'>Instagram</a></button>
+      <button className= "SocNavButton" id='linkedin' ><a href='https://www.linkedin.com/in/josephmoloughney/'>LinkedIn</a></button>
     </div>
     )
   }
