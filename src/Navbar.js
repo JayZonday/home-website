409e8f769e09e6782db676f49ab083336798e141
JMoloughney <github email address>
2018-09-21 16:32:52 -0400

    just changing navigation bar name to reflect more of what actually is

--- src/Navbar.js
+++ src/Navbar.js
@@ -5,7 +5,7 @@ class Navbar extends React.Component{
 
     return (
     <div className="navBar">
-      <h2 className="nav">Information Navigation</h2>
+      <h2 className="nav">Project Navigation</h2>
       <button className= "navButton" id='projects'><a href='https://github.com/JayZonday'>GitHub Portfolio</a></button>
       <button className= "navButton" id='youtubes'><a href='https://www.youtube.com/channel/UCPQZKbLIPNYr36qVVAwkI6Q?view_as=subscriber'>YouTube Portfolio</a></button>
       <button className= "navButton" id='ffAdvice'>FF2018 Advice</button>
